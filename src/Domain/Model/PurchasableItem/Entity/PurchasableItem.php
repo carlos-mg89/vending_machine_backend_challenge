5cddefb01127e600251f619883b1d2575e003fc1
Carlos Martínez Gadea <carlosmartinezgadea@gmail.com>
2022-07-07 16:59:44 +0200

    Added PurchasableItem entity, repository interface and Doctrine repository implementation

---
+++ src/Domain/Model/PurchasableItem/Entity/PurchasableItem.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Domain\Model\PurchasableItem\Entity;
+
+use App\Infrastructure\Repository\DoctrinePurchasableItemRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=DoctrinePurchasableItemRepository::class)
+ */
+class PurchasableItem
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=false)
+     */
+    private string $selector;
+
+    /**
+     * @ORM\Column(type="float", nullable=false)
+     */
+    private float $price;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSelector(): ?string
+    {
+        return $this->selector;
+    }
+
+    public function setSelector(string $selector): self
+    {
+        $this->selector = $selector;
+
+        return $this;
+    }
+
+    public function getPrice(): ?string
+    {
+        return $this->price;
+    }
+
+    public function setPrice(string $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+}
---
+++ src/Domain/Model/PurchasableItem/Repository/PurchasableItemRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Domain\Model\PurchasableItem\Repository;
+
+use App\Domain\Model\PurchasableItem\Entity\PurchasableItem;
+
+interface PurchasableItemRepositoryInterface
+{
+    public function add(PurchasableItem $entity, bool $flush = true): void;
+
+    public function save(PurchasableItem $entity, bool $flush = true): void;
+
+    public function remove(PurchasableItem $entity, bool $flush = true): void;
+}
