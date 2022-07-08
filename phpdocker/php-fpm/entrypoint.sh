f172a59b565e594c964628a046d0ca5cf1c2e58a
Carlos Martínez Gadea <carlosmartinezgadea@gmail.com>
2022-07-08 17:16:13 +0200

    Load Doctrine fixtures when starting Docker services

--- phpdocker/php-fpm/entrypoint.sh
+++ phpdocker/php-fpm/entrypoint.sh
@@ -14,4 +14,10 @@ php bin/console doctrine:migrations:migrate
 echo -e "\nRun DB migrations on TEST environment"
 php bin/console --env test doctrine:migrations:migrate
 
+echo -e "\nLoad DB fixtures on DEV environment"
+php bin/console doctrine:fixtures:load --no-interaction
+
+echo -e "\nLoad DB fixtures on TEST environment"
+php bin/console --env test doctrine:fixtures:load --no-interaction
+
 /usr/sbin/php-fpm8.1
